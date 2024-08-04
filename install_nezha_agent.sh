5fe886db6cff627f6fe2453ad6e90d5a4649d5ee
xsmzdd <111579783+xsmzdd@users.noreply.github.com>
2024-08-04 14:40:48 +0800

    Update install_nezha_agent.sh

--- install_nezha_agent.sh
+++ install_nezha_agent.sh
@@ -27,7 +27,7 @@ fi
 case $architecture in
     x86_64)
         echo "检测到AMD架构，下载并安装nezha-agent..."
-        wget https://github.com/nezhahq/agent/releases/download/v0.18.5/nezha-agent_linux_amd64.zip \
+        wget https://github.com/nezhahq/agent/releases/download/v0.15.15/nezha-agent_linux_amd64.zip \
         && unzip nezha-agent_linux_amd64.zip \
         && rm nezha-agent_linux_amd64.zip \
         && mv nezha-agent /opt/nezha/agent/nezha-agent \
@@ -35,7 +35,7 @@ case $architecture in
         ;;
     arm64|aarch64)
         echo "检测到ARM64架构，下载并安装nezha-agent..."
-        wget https://github.com/nezhahq/agent/releases/download/v0.18.5/nezha-agent_linux_arm64.zip \
+        wget https://github.com/nezhahq/agent/releases/download/v0.15.15/nezha-agent_linux_arm64.zip \
         && unzip nezha-agent_linux_arm64.zip \
         && rm nezha-agent_linux_arm64.zip \
         && mv nezha-agent /opt/nezha/agent/nezha-agent \
