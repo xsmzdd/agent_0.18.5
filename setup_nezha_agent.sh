0a1bbaa4317cdace7dc5ebb1a24fa372fe73372a
xsmzdd <111579783+xsmzdd@users.noreply.github.com>
2025-02-01 11:39:33 +0800

    setup_nezha_agent.sh

--- setup_nezha_agent.sh
+++ setup_nezha_agent.sh
@@ -1,16 +1,28 @@
 #!/bin/sh
 
 # 定义变量
+DOWNLOAD_URL="https://github.com/nezhahq/agent/releases/download/v0.18.5/nezha-agent_linux_amd64.zip"
 ZIP_FILE="nezha-agent_linux_amd64.zip"
 TARGET_DIR="/opt/nezha/agent/"
 SERVICE_NAME="nezha-agent"
 SERVICE_SCRIPT="/etc/init.d/${SERVICE_NAME}"
 
+# 安装必要的工具
+apk add --no-cache wget unzip
+
+# 下载文件
+echo "Downloading ${DOWNLOAD_URL}..."
+wget -O ${ZIP_FILE} ${DOWNLOAD_URL}
+
 # 解压缩文件
+echo "Unzipping ${ZIP_FILE}..."
 unzip ${ZIP_FILE}
 
 # 删除压缩包
 rm -f ${ZIP_FILE}
+
+# 创建目标目录（如果不存在）
+mkdir -p ${TARGET_DIR}
 
 # 移动Nezha Agent到目标目录
 mv nezha-agent ${TARGET_DIR}
